be6ad0b6ef5719e6eb5a36d9a237c3fd799f498c
Akshath Kamath <akshath.kamath@techmatters.com>
2024-02-08 19:10:19 +0530

    Binary Search Problems

---
+++ Binary Search/1D/upper_bound.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int lowerBound(vector<int> &nums, int n, int k)
+{
+
+    int low = 0, high = n - 1;
+    int ans = n;
+
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        if (nums[mid] <= k)
+        {
+            low = mid + 1;
+        }
+        else
+        {
+            ans = mid;
+            high = mid - 1;
+        }
+    }
+    return ans;
+}
+
+int main()
+{
+    vector<int> arr1 = {1, 2, 3, 4, 5, 5, 7, 8, 9, 10};
+    int n1 = arr1.size();
+
+    int ans = lowerBound(arr1, n1, 5);
+    cout << ans;
+
+    return 0;
+}
